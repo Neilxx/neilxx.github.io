756eca37dcd633398ea0eaa2ad6b3a11d4631100
dajing <wer321011@gmail.com>
2021-06-21 23:39:55 +0800

    improve performance

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $("#comment-img").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#fb-comments").offset().top - 100
-    }, 1500, 'easeOutQuart');
-  });
-});
